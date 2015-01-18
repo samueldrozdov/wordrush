ff20b88996de203152dbd470c033af2c23ab8c6e
koroket <bmasakazu@gmail.com>
2015-01-18 15:31:26 -0800

    timer added

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 $(function() {
 
+	start()
+});
+
+function start(){
+	var myVar = setInterval(function(){myTimer()},1000);
+
+	const timeLimit = 4;
+	var timeLeft = timeLimit;
+
+	function myTimer() {
+	    timeLeft -= 1
+	    $("#timer").html(""+timeLeft+"")
+	    if(0===timeLeft)
+	    {
+	    	clearInterval(myVar)
+	    	timeOver()
+	    }
+	}
+	function timeOver()
+	{
+		$("#timer").html("u ded")
+	}
 }
